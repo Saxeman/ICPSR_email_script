c6ac424369783fe6e32c632c044716e94c05eaeb
Saxeman <45017284+Saxeman@users.noreply.github.com>
2021-08-10 13:05:09 -0700

    Updated Main.py

--- main.py
+++ main.py
@@ -42,9 +42,8 @@ something you know (password) and something you have (a device enrolled in Duo).
 required to access to access Summer Program Canvas sites. For a description of Duo enrollment options and enrollment 
 instructions for each option, visit https://documentation.its.umich.edu/2fa/options-two-factor-authentication. 
 To enroll a device in Duo or to manage your Duo devices, visit 
-U-M Account Management at https://password.it.umich.edu.<br>
-<br>
-</p><b>We strongly encourage all faculty and instructional aides</b> to visit the Summer Program Instructional Staff 
+U-M Account Management at https://password.it.umich.edu.
+<p><b>We strongly encourage all faculty and instructional aides</b> to visit the Summer Program Instructional Staff 
 Canvas site and review the module titled Instructor Preparation - Information Technology 
 (https://umich.instructure.com/courses/485137/modules). There you will find important information about your 
 U-M account, Canvas, Zoom and additional steps that you must take to prepare for the Program.</p>
@@ -58,29 +57,20 @@ review, expulsion from the University, termination of employment, legal action o
 For information about responsible and appropriate use, see https://it.umich.edu/information-technology-policies.</p>
 <p>If you have questions about the contents of this message, or if you are in need of Summer Program computing support, 
 send email to icpsr-sptechsupp@umich.edu.</p>
-<p>Welcome to the ICPSR Summer Program!</p></body></html>
+<p>Welcome to the ICPSR Summer Program!</p>
 """
 participant_msg = """\
-Content-Type: text/html
-From: {}
-To: {}
-Subject: ICPSR Summer Program: Information Technology, Participant Preparation
-This message is sent to you on behalf of ICPSR Summer Program Computing Support. Please retain it for future 
-reference.<br>
-<br>
-To ensure that messages from ICPSR Summer Program Computing Support are not flagged as spam by your email application, 
-please add the following email address to your contact list: icpsr-sptechsupp@umich.edu.<br> 
-<br>
-We are writing to provide you with important information about the University of Michigan account credentials that 
-you will use to participate in your short workshop(s).<br>
-<br>
-A U-M uniqname, UMICH password, and enrollment in Duo two-factor authentication are required for participation in 
-the ICPSR Summer Program.<br>
-<br>
-If you are not a regular University faculty, staff or student and if you have not already received a U-M uniqname, 
+<p>This message is sent to you on behalf of ICPSR Summer Program Computing Support. Please retain it for future 
+reference.</p>
+<p>To ensure that messages from ICPSR Summer Program Computing Support are not flagged as spam by your email application, 
+please add the following email address to your contact list: icpsr-sptechsupp@umich.edu.</p>
+<p>We are writing to provide you with important information about the University of Michigan account credentials that 
+you will use to participate in your short workshop(s).</p>
+<p>A U-M uniqname, UMICH password, and enrollment in Duo two-factor authentication are required for participation in 
+the ICPSR Summer Program.</p>
+<b>If you are not a regular University faculty, staff or student</b> and if you have not already received a U-M uniqname, 
 you will receive an additional email message from icpsr-sptechsupp@umich.edu that will contain your U-M uniqname 
 (the U-M term for a user name). Upon receipt of your U-M uniqname, complete the following steps:<br>
-<br>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1. <b>Set your UMICH password</b>. To set your password, visit 
 https://password.it.umich.edu/password/forgot and enter your uniqname. When prompted to verify your identity, 
 enter the email address that you provided when you registered for the ICPSR Summer Program (i.e., the email 
@@ -92,47 +82,34 @@ two-factor authentication (2FA). Two-factor authentication requires that you pro
 something you know (password) and something you have (a device enrolled in Duo). Duo two-factor authentication 
 is required to access to access Summer Program Canvas sites. For a description of Duo enrollment options and enrollment 
 instructions for each option, visit https://documentation.its.umich.edu/2fa/options-two-factor-authentication. 
-To enroll a device in Duo or to manage your Duo devices, visit U-M Account Management at https://password.it.umich.edu.
-<br>
-<b>All participants must</b> visit the main Summer Program Canvas site and review the module titled 
+To enroll a device in Duo or to manage your Duo devices, visit 
+U-M Account Management at https://password.it.umich.edu.
+<p><b>All participants must</b> visit the main Summer Program Canvas site and review the module titled 
 Participant Preparation - Information Technology (https://umich.instructure.com/courses/483457/modules). 
 There you will find important information about your U-M account, instructional resources, software and 
-additional steps that you must take to prepare for participation in the Program.<br>
-<i>Failure to complete these tasks in advance will impede your participation in Summer Program instructional 
-activities</i>.<br>
-<br>
-Your access to Summer Program Canvas resources will expire at 12:00 AM Eastern Daylight Time (EDT) 
+additional steps that you must take to prepare for participation in the Program.</p>
+<p><i>Failure to complete these tasks in advance will impede your participation in Summer Program instructional 
+activities</i>.</p>
+<p>Your access to Summer Program Canvas resources will expire at 12:00 AM Eastern Daylight Time (EDT) 
 14 days after the last day of your workshop/course/lecture unless your instructor has requested different arrangements. 
 Please consult your instructor to confirm the period of extended access. When the period of extended 
-access expires, you will lose access to U-M Canvas and all related materials.<br>
-<br>
-The Responsible Use of Information Resources [Standard Practice Guide (SPG) 601.07] applies to all members of the 
+access expires, you will lose access to U-M Canvas and all related materials.</p>
+<p>The Responsible Use of Information Resources [Standard Practice Guide (SPG) 601.07] applies to all members of the 
 University community and refers to all information resources (http://spg.umich.edu/policy/601.07). By using the 
 university's technology services, you agree to follow U-M information technology policies and guidelines for 
 responsible use. Inappropriate use of U-M technology resources may result in termination of access, disciplinary 
 review, expulsion from the University, termination of employment, legal action or other disciplinary action. 
-For information about responsible and appropriate use, see https://it.umich.edu/information-technology-policies.<br>
-<br>
-If you have questions about the contents of this message, or if you are in need of Summer Program computing support, 
-send email to icpsr-sptechsupp@umich.edu.<br>
-<br>
-Welcome to the ICPSR Summer Program!<br>
-<br>
-"""
-compsupp_msg = """\
-Content-Type: text/html
-From: {}
-To: {}
-Subject: 
-This is a test message because I dont have the boilerplate email yet, here is a link to the documentation homepage 
-www.google.com<br>
-<br>
+For information about responsible and appropriate use, see https://it.umich.edu/information-technology-policies.</p>
+<p>If you have questions about the contents of this message, or if you are in need of Summer Program computing support, 
+send email to icpsr-sptechsupp@umich.edu.</p>
+<p>Welcome to the ICPSR Summer Program!</p>
 """
 username_msg = """\
 Content-Type: text/html
 From: {}
 To: {}
 Subject: ICPSR Summer Program Computing Support Message
+<body><html>
 Dear {} {},<br>
 <br>
 Welcome to the ICPSR Summer Program.<br>
@@ -147,6 +124,7 @@ Content-Type: text/html
 From: {}
 To: {}
 Subject: ICPSR Summer Program Computing Support Message    
+</body></html>
 Dear {} {},<br>
 <br>
 Welcome to the ICPSR Summer Program.<br>
@@ -168,14 +146,13 @@ Project Manager, ICPSR Summer Program Computing Services<br>
 Computing Support Services<br>
 ICPSR Summer Program in Quantitative Methods of Social Research<br>
 Inter-university Consortium for Political and Social Research<br>
-University of Michigan"""
+University of Michigan</body></html>"""
 
 context = ssl.create_default_context()
 
 parser = argparse.ArgumentParser(description='ICPSR Email Sender')
 parser.add_argument("-f", "--filename", nargs=1, help="filename", required=True)
 parser.add_argument("-i", "--intro", action='store_true', help="Sends introduction email")
-parser.add_argument("-c", "--compsupp", action='store_true', help="Sends support documentation email")
 parser.add_argument("-u", "--send_username", action='store_true', help="Sends username to participant")
 parser.add_argument("-p", "--send_password", action='store_true', help="Sends password to participant")
 
@@ -199,30 +176,6 @@ intro_msg_instructor.attach(msg_content)
 # Creates the participant intro email
 intro_msg_participant = MIMEMultipart()
 intro_msg_participant['From'] = sender_icpsr
-intro_msg_participant['Subject'] = "ICPSR Summer Program: Information Technology, Participant Preparation"
-msg_content = MIMEText('<html><body><p><img src="cid:0"></p>' + participant_msg + signature, 'html', 'utf-8')
-intro_msg_participant.attach(msg_content)
-
-# Creates the password email
-intro_msg_participant = MIMEMultipart()
-intro_msg_participant['From'] = sender_icpsr
-intro_msg_participant['To'] = receiver
-intro_msg_participant['Subject'] = "ICPSR Summer Program: Information Technology, Participant Preparation"
-msg_content = MIMEText('<html><body><p><img src="cid:0"></p>' + participant_msg + signature, 'html', 'utf-8')
-intro_msg_participant.attach(msg_content)
-
-# Creates the username email
-intro_msg_participant = MIMEMultipart()
-intro_msg_participant['From'] = sender_icpsr
-intro_msg_participant['To'] = receiver
-intro_msg_participant['Subject'] = "ICPSR Summer Program: Information Technology, Participant Preparation"
-msg_content = MIMEText('<html><body><p><img src="cid:0"></p>' + participant_msg + signature, 'html', 'utf-8')
-intro_msg_participant.attach(msg_content)
-
-# Creates the compsupp email
-intro_msg_participant = MIMEMultipart()
-intro_msg_participant['From'] = sender_icpsr
-intro_msg_participant['To'] = receiver
 intro_msg_participant['Subject'] = "ICPSR Summer Program: Information Technology, Participant Preparation"
 msg_content = MIMEText('<html><body><p><img src="cid:0"></p>' + participant_msg + signature, 'html', 'utf-8')
 intro_msg_participant.attach(msg_content)
@@ -261,11 +214,6 @@ with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
                     log = {'input_error': '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())}
                     logfile.write(str(log) + '\n')
                     exit(1)
-            if args.compsupp:
-                compsupp_msg = compsupp_msg.format(sender_icpsr, receiver)
-                server.sendmail(sender, receiver, compsupp_msg + signature)
-                log = {'compsupp_message': '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())}
-                logfile.write(str(log) + '\n')
             if args.send_username:
                 if exclude in row[2]:
                     log = {'umich_error': '{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now())}
